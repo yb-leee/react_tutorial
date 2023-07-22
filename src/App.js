0036e245d812bbce501de44a3ad16039d889563e
“Ryan <sungjin.712@navercorp.com>
2023-07-22 21:16:50 +0900

    함수의 리턴 값에서 중괄호를 쓰면 변수로 간주된다.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      hi
+function Counter(props){
+  console.log(props.title);
+  return <div>
+    <h1> {props.title}</h1>
+    <button>+</button> {props.initValue}
+  </div>
+}
+function App(){
+  return(
+    <div>
+      <Counter title = "불면증 카운터" initValue = {10}></Counter>
+      <Counter title = "손님 카운터" initValue = {10}></Counter>
     </div>
   );
-}
+  }
 
 export default App;
